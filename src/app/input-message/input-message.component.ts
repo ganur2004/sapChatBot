f6fc89941c77d6b94f1dcf45aba7334ec9b682c5
nurmatgabdollaev <nurmat.kanatuli@gmail.com>
2024-11-08 01:10:34 +0500

    add input-message

--- src/app/input-message/input-message.component.ts
+++ src/app/input-message/input-message.component.ts
@@ -41,37 +41,98 @@ export class InputMessageComponent {
   isEditing: boolean = false;
   editIndex: number | null = null;
 
-  sendMessage() {
-    if (this.messageInput.trim()) {
-        if (this.isEditing) {
-            // Если мы редактируем, обновляем сообщение
-            this.chatFolders[this.currentFolderIndex].messages[this.editIndex!].text = this.messageInput;
-            this.isEditing = false; // Выходим из режима редактирования
-            this.editIndex = null; // Сбрасываем индекс редактируемого сообщения
-        } else {
-            // Добавляем новое сообщение
-            this.chatFolders[this.currentFolderIndex].messages.unshift({
-                text: this.messageInput,
-                type: 'sent'
-            });
-        }
-
-        // Устанавливаем индикатор "печати"
-        this.isTyping = true;
-
-        // Имитация задержки сервера
-        setTimeout(() => {
-            // Ответ сервера — это то же сообщение
-            this.chatFolders[this.currentFolderIndex].messages.unshift({
-                text: this.messageInput, // Можно заменить на другой текст, если нужно
-                type: 'received'
-            });
-
-            // Скрываем индикатор "печати"
-            this.isTyping = false;
-            this.messageInput = ''; // Очищаем поле ввода
-        }, 2000); // 2 секунды задержки
+  data: any = [
+    {
+        "question": "Помогите узнать решение/ответ моего инцидента/запроса?",
+        "answer": "Решения по инцидентам смотрите в ситеме SOP."
+    },
+    {
+        "question": "Не могу зайти в систему SOP",
+        "answer": "Если есть проблемы со входом в систему SOP оставьте заявку в техподдержку(Значок- желтый телефон на рабочем столе )"
+    },
+    {
+        "question": "Прошу сбросить пароль в системе SAP.",
+        "answer": "Вам необходимо обратиться в техподдержку (Значок- желтый телефон на рабочем столе)"
+    },
+    {
+        "question": "Прошу прописать согласующего в матрице по утверждению резервирований",
+        "answer": "Вам необходимо обратиться в плановый отдел (ПЭУ)."
+    },
+    {
+        "question": "Прошу прописать инициатора в матрице по созданию резервирований на сырье",
+        "answer": "Вам необходимо обратиться в плановый отдел (ПЭУ)."
+    },
+    {
+        "question": "Прошу прописать согласующего (инициатора) в матрице по переносу бюджета",
+        "answer": "Вам необходимо обратиться в плановый отдел (ПЭУ)."
+    },
+    {
+        "question": "Прошу предоставить полномочия (предоставить роли) в системе SAP",
+        "answer": "Вам необходимо создать заявку в системе QagazOptima на пользователя в системе SAP"
+    },
+    {
+        "question": "Не работает телеграм бот",
+        "answer": "Проверьте название чат бота. Должно быть QarmetKzBot, должна быть личная регистрация в боте, номер телефона предоствлен в ЦЕС."
+    },
+    {
+        "question": "Не приходят СМС сообщения",
+        "answer": "Вам необходимо обратиться к своему оператору связи"
+    },
+    {
+        "question": "Не могу зайти в систему Фиори. Выходит следующая ошибка: *Ошибка RFC пользователь блокирован в бэкенд системе*",
+        "answer": "Долговременно не было входа в систему PHK. Необходимо подать заявку в техподдержку(Значок- желтый телефон на рабочем столе)"
+    },
+    {
+        "question": "Система выдает ошибку при сторнировании",
+        "answer": "Ошибка эта вылезает скорее всего, так как после того как вы оприходовали запас, какое-то количество уже ушло дальше. То есть на складе сейчас меньше чем должно было быть. Вот и не может от сторнировать. Сначала нужно сторнировать фактуру а потом движение. В любом случае весь процесс идет с конца и по шагам откатывается в том же порядке в котором был выполнен. Все последующие документы так же должны быть сторнированы. "
     }
+]
+
+
+sendMessage() {
+  if (this.messageInput.trim()) {
+      // Проверяем, есть ли вопрос в data
+      const foundItem = this.data.find((item: any) => item.question === this.messageInput.trim());
+      
+      if (this.isEditing) {
+          // Если мы редактируем, обновляем сообщение
+          this.chatFolders[this.currentFolderIndex].messages[this.editIndex!].text = this.messageInput;
+          this.isEditing = false; // Выходим из режима редактирования
+          this.editIndex = null; // Сбрасываем индекс редактируемого сообщения
+      } else {
+          // Добавляем новое сообщение
+          this.chatFolders[this.currentFolderIndex].messages.unshift({
+              text: this.messageInput,
+              type: 'sent'
+          });
+      }
+
+      // Устанавливаем индикатор "печати"
+      this.isTyping = true;
+
+      // Имитация задержки сервера
+      setTimeout(() => {
+          let responseText;
+
+          if (foundItem) {
+              // Если нашли вопрос, возвращаем ответ из data
+              responseText = foundItem.answer;
+          } else {
+              // Иначе возвращаем введенное сообщение
+              responseText = this.messageInput; // можно изменить на произвольный ответ
+          }
+
+          // Ответ сервера
+          this.chatFolders[this.currentFolderIndex].messages.unshift({
+              text: responseText,
+              type: 'received'
+          });
+
+          // Скрываем индикатор "печати"
+          this.isTyping = false;
+          this.messageInput = ''; // Очищаем поле ввода
+      }, 2000); // 2 секунды задержки
+  }
 }
 
 editMessage(index: number) {
